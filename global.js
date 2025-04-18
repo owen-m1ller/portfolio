84e99fcde2470458f8a6738cbb135a8533abdb58
owen-m1ller <omiller@ucsd.edu>
2025-04-18 13:32:22 -0700

    test section 3

---
+++ global.js
@@ -0,0 +1,44 @@
+console.log("it's alive!");
+
+function $$(selector, context=document) {
+    return Array.from(context.querySelectorAll(selector))
+}
+
+// step 2
+// let navLinks = $$("nav a");
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+// currentLink?.classList.add("current");
+
+// step 3
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: "https://github.com/owen-m1ller", title: "My GitHub" }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/portfolio/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host != location.host) {
+        a.target = "_blank"
+    }
+    nav.append(a);
+}
