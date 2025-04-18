efec829c67f0b5a579760948bcb51033dcae448c
owen-m1ller <omiller@ucsd.edu>
2025-04-18 14:27:55 -0700

    complete lab 3

--- global.js
+++ global.js
@@ -42,3 +42,26 @@ for (let p of pages) {
     }
     nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+      <label class="color-scheme">
+          Theme:
+          <select>
+              <option value="light dark">Automatic</option>
+              <option value="light">Light</option>
+              <option value="dark">Dark</option>
+          </select>
+      </label>`,
+);
+
+let select = document.querySelector("select")
+select.addEventListener('input', function (event) {
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value;
+});
+
+if (localStorage.colorScheme) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+}
